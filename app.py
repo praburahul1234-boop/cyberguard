073dbe221ca147d1c1953aa75b0decdaffc99dfb
praburahul1234-boop <praburahul1234@gmail.com>
2026-02-06 19:34:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,5 +48,8 @@ def home():
 
     return render_template("index.html", result=result, color=color)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0, port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
