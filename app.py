f6f7f9fa363318ce2048bf25e50cf3a0234cf7cf
praburahul1234-boop <praburahul1234@gmail.com>
2026-02-06 21:37:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,6 +49,13 @@ def home():
     return render_template("index.html", result=result, color=color)
 
 import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
